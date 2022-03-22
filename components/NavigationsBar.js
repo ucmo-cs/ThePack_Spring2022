7ca27b45f5222bf7a1c5f8b27990f2d0e00bb1ea
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-03-22 14:19:55 -0500

    Added mobile togile to Navigation bar

--- components/NavigationsBar.js
+++ components/NavigationsBar.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { useState } from 'react'
 
 // const styledLink = styled.a`
 
@@ -11,28 +12,41 @@ const StyledUl = styled.ul`
     justify-content: end;
     list-style: none;
     background-color: #f4f4f3;
+    transition: 0.7s ease-in-out;
+    padding: 0;
+    margin: 0;
     @media (max-width: 768px) {
-        display: none;
+        overflow: hidden;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: ${props => props.expanded ? '90px' : '0px'};
     }
 `
 
 const StyledLi = styled.li`
     text-decoration: none;
-    padding: 0.5rem 1rem;
+    padding: 4px;
     &:hover {
         background-color: #72d0ed;
         color: #202e4a;
+    }
+
+    @media (max-width: 768px) {
+        transition: 0.7s ease-in-out;
+        height: ${props => props.expanded ? '30px' : '0px'};
+        width: 100%;
+        text-align: center;   
     }
 `
 const Hamburger = styled.div`
     display: none;
     @media (max-width: 768px) {
-        display: flex;
+        display: block;
         background-color: #E5E5E5;
         border-radius: 5px;
         flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
+        transition: 0.7s ease-in-out;
         width: 30px;
         height: 30px;
         position: relative;
@@ -48,46 +62,50 @@ const Hamburger = styled.div`
 const HamburgerLine = styled.div`
     @media (max-width: 768px) {
         background-color: #828282;
+        position: absolute;
         width: 70%;
         border-radius: 5px;
         height: 2px;
         transition: 0.7s ease-in-out;
-        top: ${props => (props.top ? '0' : 'auto')};
-        bottom: ${props => (props.bottom ? '0' : 'auto')};
-        transform: translateX(-5vw);
+        left: 4.5px;
 
         :nth-child(1) {
-            transform: ${props => (props.top ? 'rotate(45deg)' : 'rotate(-45deg)')};
+            top: 6px;
+            ${props => props.expanded && css`transform: rotate(45deg); top: 14px;`};
         }
         :nth-child(2) {
-            opacity: ${props => (props.top ? '0' : '1')};
+            top: 14px;
+            opacity: ${props => (props.expanded ? '0' : '1')};
         }
         :nth-child(3) {
-            transform: ${props => (props.top ? 'rotate(-45deg)' : 'rotate(45deg)')};
+            top: 22px;
+            ${props => props.expanded && css`transform: rotate(-225deg); top: 14px;`};
         }
     } 
 `
 
 export default function NavigationBar() {
+    const [expanded, setExpanded] = useState(false)
+
     return (
         <nav>
-            <Hamburger>
-                <HamburgerLine />
-                <HamburgerLine />
-                <HamburgerLine />
+            <Hamburger expanded={expanded} onClick={() => setExpanded(!expanded)}>
+                <HamburgerLine expanded={expanded} />
+                <HamburgerLine expanded={expanded} />
+                <HamburgerLine expanded={expanded} />
             </Hamburger>
-            <StyledUl>
-                <StyledLi>
+            <StyledUl expanded={expanded}>
+                <StyledLi expanded={expanded}>
                     <Link href='/'>
                         <a>Home</a>
                     </Link>
                 </StyledLi>
-                <StyledLi>
+                <StyledLi expanded={expanded}>
                     <Link href='/profile'>
                         <a>Profile</a>
                     </Link>
                 </StyledLi>
-                <StyledLi>
+                <StyledLi expanded={expanded}>
                     <Link href='/settings'>
                         <a>Account Settings</a>
                     </Link>
