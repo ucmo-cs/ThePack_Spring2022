7d4c74f9ba78fb1b71b09ba265299ff2fa25cec0
cpg55850 <cpg55850@ucmo.edu>
2022-03-24 15:01:41 -0500

    Converted profile page to user/[id] page and moved styling to styled components

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,32 +4,38 @@ import Image from 'next/image'
 import styled from 'styled-components'
 
 Avatar.propTypes = {
-    username: PropTypes.string.isRequired,
-    profileImageUrl: PropTypes.string.isRequired,
-    size: PropTypes.oneOf(['small', 'medium', 'large']),
+  username: PropTypes.string.isRequired,
+  profileImageUrl: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
 }
 
 const StyledImage = styled(Image)`
-    border-radius: 50%;
+  border-radius: 50%;
 `
 
 function Avatar(props) {
-    const { username, profileImageUrl, size } = props
-    let width = 0
-    if (size === 'small') {
-        width = 30
-    } else if (size === 'medium') {
-        width = 50
-    } else if (size === 'large') {
-        width = 70
-    }
-    let height = width
+  const { username, profileImageUrl, size } = props
+  let width = 0
+  if (size === 'small') {
+    width = 30
+  } else if (size === 'medium') {
+    width = 50
+  } else if (size === 'large') {
+    width = 70
+  }
+  let height = width
 
-    return (
-        <Link href={`/profile/${username}`} passHref>
-            <StyledImage width={width} height={height} src={profileImageUrl} alt={username} title={username} />
-        </Link>
-    )
+  return (
+    <Link href={`/profile/${username}`} passHref>
+      <StyledImage
+        width={width}
+        height={height}
+        src={profileImageUrl}
+        alt={username}
+        title={username}
+      />
+    </Link>
+  )
 }
 
 export default Avatar
--- components/ViewWuphf.js
+++ components/ViewWuphf.js
@@ -3,19 +3,21 @@ import styles from '../styles/ViewWuphfs.module.css'
 import Avatar from './Avatar'
 
 function ViewWuphf(props) {
-	return (
-		<div className={styles.container}>
-			<span className={styles.avatar}>
-				<Avatar
-					username={props.username}
-					profileImageUrl={props.avatar}
-					size='medium'
-				/>
-				<span className={styles.username}>{props.username}</span>
-			</span>
-			<div className={styles.post}>{props.post}</div>
-		</div>
-	)
+  return (
+    <div className={styles.container}>
+      <div className={styles.avatar}>
+        <Avatar
+          username={props.username}
+          profileImageUrl={props.avatar}
+          size="medium"
+        />
+      </div>
+      <div className={styles.postWrapper}>
+        <h3 className={styles.username}>{props.username}</h3>
+        <p className={styles.post}>{props.post}</p>
+      </div>
+    </div>
+  )
 }
 
 export default ViewWuphf
