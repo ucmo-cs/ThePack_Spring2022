6f8352f7b21ae97365589eb8dd111eb664bcc6d1
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-03-24 15:53:21 -0500

    Added official logo and cleaned up code

--- components/NavigationsBar.js
+++ components/NavigationsBar.js
@@ -4,6 +4,73 @@ import { useState, useRef } from 'react'
 import FormInput from './forms/FormInput'
 import { useForm } from 'react-hook-form'
 
+const avatar = <img src='https://res.cloudinary.com/wuphf/image/upload/v1647982586/animal_svgs/dog_nau7in.svg' width={40} height={40} />
+
+export default function NavigationBar() {
+    const [expanded, setExpanded] = useState(false)
+    const myRef = useRef(null)
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+        watch
+    } = useForm({
+        mode: 'onTouched',
+    })
+
+    function toggleExpanded(e) {
+        if (!expanded) {
+            myRef.current.scrollIntoView()
+        }
+        setExpanded(!expanded)
+    }
+
+    return (
+        <NavWrapper>
+            <StyledNav expanded={expanded} ref={myRef}>
+                <TopWrapper>
+                    {avatar}
+                    <Hamburger expanded={expanded} onClick={toggleExpanded}>
+                        <HamburgerLine expanded={expanded} />
+                        <HamburgerLine expanded={expanded} />
+                        <HamburgerLine expanded={expanded} />
+                    </Hamburger>
+                </TopWrapper>
+                <DesktopLinksListWrapper>
+                    <DesktopLogoWrapper>
+                        {avatar}
+                    </DesktopLogoWrapper>
+                    <Search>
+                        <FormInput
+                            id='search'
+                            label=''
+                            register={register}
+                            error={errors.search}
+                        />
+                    </Search>
+                    <StyledUl expanded={expanded}>
+                        <StyledLi expanded={expanded}>
+                            <Link href='/' passHref>
+                                <StyledA>Home</StyledA>
+                            </Link>
+                        </StyledLi>
+                        <StyledLi expanded={expanded}>
+                            <Link href='/profile' passHref>
+                                <StyledA>Profile</StyledA>
+                            </Link>
+                        </StyledLi>
+                        <StyledLi expanded={expanded}>
+                            <Link href='/settings' passHref>
+                                <StyledA>Account Settings</StyledA>
+                            </Link>
+                        </StyledLi>
+                    </StyledUl>
+                </DesktopLinksListWrapper>
+            </StyledNav>
+        </NavWrapper>
+    )
+}
+
 const NavWrapper = styled.div`
     width: 100%;
     background-color: #E5E5E5;
@@ -162,68 +229,3 @@ const HamburgerLine = styled.div`
         }
     } 
 `
-
-export default function NavigationBar() {
-    const [expanded, setExpanded] = useState(false)
-    const myRef = useRef(null)
-    const {
-        register,
-        handleSubmit,
-        formState: {errors, isSubmitting},
-        watch
-    } = useForm({
-        mode: 'onTouched',
-    })
-
-    function toggleExpanded(e) {
-        if (!expanded) {
-            myRef.current.scrollIntoView()
-        }
-        setExpanded(!expanded)
-    }
-
-    return (
-        <NavWrapper>
-            <StyledNav expanded={expanded} ref={myRef}>
-                <TopWrapper>
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path fill="#828282" d="M9 23h-6v-10l8.991-8.005 9.009 8.005v10h-6v-3c0-1.654-1.355-3.021-3.009-3.021-1.655 0-2.991 1.367-2.991 3.021v3zm2.252-11.015c.094-.002 1.385-.002 1.477 0 1.17.016 1.264-.998 2.259-.998.643 0 .995.524.999.999.005.474-.28.825-.622.995.327.177.619.527.622 1.002.003.475-.347.999-.999.999-.995 0-1.089-1.015-2.259-.998h-1.477c-1.17-.017-1.264.998-2.259.998-.652 0-1.002-.524-.999-.999.003-.475.295-.825.622-1.002-.342-.17-.627-.521-.622-.995.004-.475.356-.999.999-.999.995 0 1.089 1.014 2.259.998zm.748-10.985l12 10.632-1.328 1.493-10.672-9.481-10.672 9.481-1.328-1.481 12-10.644z" /></svg>
-                    <Hamburger expanded={expanded} onClick={toggleExpanded}>
-                        <HamburgerLine expanded={expanded} />
-                        <HamburgerLine expanded={expanded} />
-                        <HamburgerLine expanded={expanded} />
-                    </Hamburger>
-                </TopWrapper>
-                <DesktopLinksListWrapper>
-                    <DesktopLogoWrapper>
-                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path fill="#828282" d="M9 23h-6v-10l8.991-8.005 9.009 8.005v10h-6v-3c0-1.654-1.355-3.021-3.009-3.021-1.655 0-2.991 1.367-2.991 3.021v3zm2.252-11.015c.094-.002 1.385-.002 1.477 0 1.17.016 1.264-.998 2.259-.998.643 0 .995.524.999.999.005.474-.28.825-.622.995.327.177.619.527.622 1.002.003.475-.347.999-.999.999-.995 0-1.089-1.015-2.259-.998h-1.477c-1.17-.017-1.264.998-2.259.998-.652 0-1.002-.524-.999-.999.003-.475.295-.825.622-1.002-.342-.17-.627-.521-.622-.995.004-.475.356-.999.999-.999.995 0 1.089 1.014 2.259.998zm.748-10.985l12 10.632-1.328 1.493-10.672-9.481-10.672 9.481-1.328-1.481 12-10.644z" /></svg>
-                    </DesktopLogoWrapper>
-                    <Search>
-                        <FormInput
-                            id='search'
-                            label=''
-                            register={register}
-                            error={errors.search}
-                        />
-                    </Search>
-                    <StyledUl expanded={expanded}>
-                        <StyledLi expanded={expanded}>
-                            <Link href='/' passHref>
-                                <StyledA>Home</StyledA>
-                            </Link>
-                        </StyledLi>
-                        <StyledLi expanded={expanded}>
-                            <Link href='/profile' passHref>
-                                <StyledA>Profile</StyledA>
-                            </Link>
-                        </StyledLi>
-                        <StyledLi expanded={expanded}>
-                            <Link href='/settings' passHref>
-                                <StyledA>Account Settings</StyledA>
-                            </Link>
-                        </StyledLi>
-                    </StyledUl>
-                </DesktopLinksListWrapper>
-            </StyledNav>
-        </NavWrapper>
-    )
-}
