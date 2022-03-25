6ef534830fa63fcce77cfd70a7804208e8223629
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-03-25 17:15:53 -0500

    Changed SettingsForm to Avatar to change when select box is changed

--- pages/register.js
+++ pages/register.js
@@ -1,22 +1,101 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SettingsForm from '../components/SettingsForm'
 import styled from 'styled-components'
 import Avatar from '../components/Avatar'
 import breakpoint from '../styles/breakpoint'
 import Container from '../components/styledComponents/Container'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup.object({
+	username: yup.string().min(4, 'Minimum length is 4').required('Required'),
+	animal: yup.string().required('Required'),
+	message: yup.string().required('Required'),
+})
+
 
 export default function Register() {
+	// TODO: Replace with an actual database response for all animal types
+	const animals = [
+		{
+			link: 'animal_svgs/dog_nau7in.svg',
+			label: 'Dog'
+		},
+		{
+			link: 'animal_svgs/cat_hizjv6.svg',
+			label: 'Cat'
+		},
+		{
+			link: 'animal_svgs/bird_wlfceb.svg',
+			label: 'Bird'
+		},
+		{
+			link: 'animal_svgs/owl_xnejqi.svg',
+			label: 'Owl'
+		},
+		{
+			link: 'animal_svgs/monkey_ywewbg.svg',
+			label: 'Monkey'
+		},
+		{
+			link: 'animal_svgs/bunny_tgvcdh.svg',
+			label: 'Bunny'
+		},
+		{
+			link: 'animal_svgs/panda_fb7grl.svg',
+			label: 'Panda'
+		}
+	]
+	const [selectedAnimal, setSelectedAnimal] = useState(animals[0])
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+		watch
+	} = useForm({
+		mode: 'onTouched',
+		resolver: yupResolver(schema),
+		defaultValues: {
+			selectedAnimal: animals[0],
+		}
+	})
+
+	useEffect(() => {
+		const subscription = watch((value, { name, type }) => {
+			let newAnimal = value
+			for (let i = 0; i < animals.length; i++) {
+				if (animals[i].label.toUpperCase() === watch('animal').toUpperCase()) {
+					newAnimal = animals[i]
+					break
+				}
+			}
+			console.log(newAnimal)
+			setSelectedAnimal(newAnimal)
+		})
+		return () => subscription.unsubscribe()
+	}, watch)
+
 	return (
 		<Container>
 			<Wrapper>
 				<div className='avatarContainer'>
 					<Avatar
 						username='Joe Smith'
-						profileImageUrl='sample.jpg'
+						profileImageUrl={selectedAnimal.link}
 						size='large'
 					/>
 				</div>
-				<SettingsForm />
+				<SettingsForm
+					selectedAnimal={selectedAnimal}
+					setSelectedAnimal={setSelectedAnimal}
+					animals={animals}
+					handleSubmit={handleSubmit}
+					errors={errors}
+					watch={watch}
+					isSubmitting={isSubmitting}
+					register={register}
+				/>
 			</Wrapper>
 		</Container>
 	)
