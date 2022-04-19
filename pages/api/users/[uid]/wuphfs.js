0b2406dc75901a7d2f1729de25bbd0346a3c689f
Charles Graham <cpg55850@ucmo.edu>
2022-04-19 15:11:41 -0500

    Users wuphfs are in desc order in API

--- pages/api/users/[uid]/wuphfs.js
+++ pages/api/users/[uid]/wuphfs.js
@@ -1,34 +1,37 @@
 import { prisma } from '../../../../lib/prisma'
 
 export default async function handler(req, res) {
-  const { uid } = req.query
+	const { uid } = req.query
 
-  // /users/[uid]/wuphfs
-  if (req.method === 'GET') {
-    try {
-      const wuphfs = await prisma.Wuphf.findMany({
-        where: {
-          userId: uid,
-        },
-        include: {
-          _count: {
-            select: {
-              Likes: true,
-              Comments: true,
-            },
-          },
-        },
-      })
+	// /users/[uid]/wuphfs
+	if (req.method === 'GET') {
+		try {
+			const wuphfs = await prisma.Wuphf.findMany({
+				where: {
+					userId: uid,
+				},
+				include: {
+					_count: {
+						select: {
+							Likes: true,
+							Comments: true,
+						},
+					},
+				},
+				orderBy: {
+					createdAt: 'desc',
+				},
+			})
 
-      if (wuphfs.length === 0) {
-        return res.status(404).json({ msg: `No Wuphfs by ${uid} found` })
-      }
+			if (wuphfs.length === 0) {
+				return res.status(404).json({ msg: `No Wuphfs by ${uid} found` })
+			}
 
-      res.json(wuphfs)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error })
-      throw error
-    }
-  }
+			res.json(wuphfs)
+		} catch (error) {
+			console.error(error)
+			res.status(500).json({ error })
+			throw error
+		}
+	}
 }
--- pages/api/wuphfs/[wid]/index.js
+++ pages/api/wuphfs/[wid]/index.js
@@ -1,89 +1,89 @@
 import { Prisma, prisma } from '../../../../lib/prisma'
 
 export default async function handler(req, res) {
-  const { wid } = req.query
+	const { wid } = req.query
 
-  // /wuphfs/[wid]
-  if (req.method === 'GET') {
-    try {
-      const wuphf = await prisma.Wuphf.findUnique({
-        where: {
-          id: Number(wid),
-        },
-        include: {
-          _count: {
-            select: {
-              Likes: true,
-              Comments: true,
-            },
-          },
-        },
-      })
+	// /wuphfs/[wid]
+	if (req.method === 'GET') {
+		try {
+			const wuphf = await prisma.Wuphf.findUnique({
+				where: {
+					id: Number(wid),
+				},
+				include: {
+					_count: {
+						select: {
+							Likes: true,
+							Comments: true,
+						},
+					},
+				},
+			})
 
-      if (!wuphf) {
-        return res.status(404).json({ msg: 'Wuphf not found' })
-      }
+			if (!wuphf) {
+				return res.status(404).json({ msg: 'Wuphf not found' })
+			}
 
-      res.json(wuphf)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error })
-      throw error
-    }
-  } else if (req.method === 'PATCH') {
-    try {
-      const wuphf = await prisma.Wuphf.update({
-        where: {
-          id: Number(wid),
-        },
-        data: {
-          postBody: req.body.postBody,
-          pictureUrl: req.body.pictureUrl || undefined,
-        },
-      })
+			res.json(wuphf)
+		} catch (error) {
+			console.error(error)
+			res.status(500).json({ error })
+			throw error
+		}
+	} else if (req.method === 'PATCH') {
+		try {
+			const wuphf = await prisma.Wuphf.update({
+				where: {
+					id: Number(wid),
+				},
+				data: {
+					postBody: req.body.postBody,
+					pictureUrl: req.body.pictureUrl || undefined,
+				},
+			})
 
-      res.json(wuphf)
-    } catch (error) {
-      // P2025
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        // The .code property can be accessed in a type-safe manner
-        if (error.code === 'P2025') {
-          return res
-            .status(404)
-            .json({ msg: 'The Wuphf to update was not found' })
-        }
-      }
-      console.error(error)
-      res.status(500).json({ error })
-      throw error
-    }
+			res.json(wuphf)
+		} catch (error) {
+			// P2025
+			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				// The .code property can be accessed in a type-safe manner
+				if (error.code === 'P2025') {
+					return res
+						.status(404)
+						.json({ msg: 'The Wuphf to update was not found' })
+				}
+			}
+			console.error(error)
+			res.status(500).json({ error })
+			throw error
+		}
 
-    // #authorization - who is allowed to do this?
-    // #validation - invalid input
-  } else if (req.method === 'DELETE') {
-    try {
-      const wuphf = await prisma.Wuphf.delete({
-        where: {
-          id: Number(wid),
-        },
-      })
+		// #authorization - who is allowed to do this?
+		// #validation - invalid input
+	} else if (req.method === 'DELETE') {
+		try {
+			const wuphf = await prisma.Wuphf.delete({
+				where: {
+					id: Number(wid),
+				},
+			})
 
-      res.json(wuphf)
-    } catch (error) {
-      // P2025
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        // The .code property can be accessed in a type-safe manner
-        if (error.code === 'P2025') {
-          return res
-            .status(404)
-            .json({ msg: 'The Wuphf to delete was not found' })
-        }
-      }
-      console.error(error)
-      res.status(500).json({ error })
-      throw error
-    }
+			res.json(wuphf)
+		} catch (error) {
+			// P2025
+			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				// The .code property can be accessed in a type-safe manner
+				if (error.code === 'P2025') {
+					return res
+						.status(404)
+						.json({ msg: 'The Wuphf to delete was not found' })
+				}
+			}
+			console.error(error)
+			res.status(500).json({ error })
+			throw error
+		}
 
-    // #authorization - who is allowed to do this?
-  }
+		// #authorization - who is allowed to do this?
+	}
 }
