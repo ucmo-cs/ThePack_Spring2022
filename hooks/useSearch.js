fe9ececd5e0af47cf94b43971bde8d4173453a0c
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2022-04-20 10:31:24 -0500

    Removed mockApiCall

--- hooks/useSearch.js
+++ hooks/useSearch.js
@@ -5,68 +5,52 @@ import { useForm } from 'react-hook-form'
 import axios from 'axios'
 
 export function useSearch() {
-	const [result, setResult] = useState([])
-	const {
-		register,
-		formState: { errors },
-		handleSubmit,
-		watch,
-		setValue,
-	} = useForm()
-	const [isHidden, setIsHidden] = useState(true)
+    const [result, setResult] = useState([])
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        watch,
+        setValue,
+    } = useForm()
+    const [isHidden, setIsHidden] = useState(true)
 
-	useEffect(async () => {
-		const subscription = watch(async (value) => {
-			if (value.search === '') {
-				setResult([])
-				setIsHidden(true)
-			} else {
-				setIsHidden(false)
-				const users = (await axios.get('/api/search', {
-					params: {
-						userName: value.search,
-					}
-				})).data.slice(0, 10).map(user => {
-					return {
-						username: user.userName, 
-						avatar: user.avatar
-					}
-				})
+    useEffect(async() => {
+        const subscription = watch(async(value) => {
+            if (value.search === '') {
+                setResult([])
+                setIsHidden(true)
+            } else {
+                setIsHidden(false)
+                const users = (await axios.get('/api/search', {
+                    params: {
+                        userName: value.search,
+                    }
+                })).data.slice(0, 10).map(user => {
+                    return {
+                        username: user.userName,
+                        avatar: user.avatar
+                    }
+                })
 
-				handleSubmit(setResult(users))
-				if(users.length === 0) {
-					setIsHidden(true)
-				}
-			}
-		})
-		return () => subscription.unsubscribe()
-	}, [watch])
+                handleSubmit(setResult(users))
+                if (users.length === 0) {
+                    setIsHidden(true)
+                }
+            }
+        })
+        return () => subscription.unsubscribe()
+    }, [watch])
 
-	function hide() {
-		setIsHidden(true)
-	}
+    function hide() {
+        setIsHidden(true)
+    }
 
-	return {
-		query: errors.search,
-		register,
-		result,
-		isHidden,
-		hide,
-	}
-}
-
-function mockAPICall() {
-	const data = dummySearchResults
-
-	const result = [data[Math.floor(Math.random() * data.length)]]
-	for (let i = 0; i < data.length; i++) {
-		if (Math.floor(Math.random() * 2) === 0) {
-			const entry = data[i]
-			if (entry !== result[0]) {
-				result.push(entry)
-			}
-		}
-	}
-
-	return result
+    return {
+        query: errors.search,
+        register,
+        result,
+        isHidden,
+        hide,
+    }
 }
