eaa2ac1b959e631afcecd4b35483757dd0353b11
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-04-20 20:38:08 -0500

    Updated prisma migration for non-nullable column

--- prisma/migrations/20220420234043_add_theme_to_wuphf_user/migration.sql
+++ prisma/migrations/20220420234043_add_theme_to_wuphf_user/migration.sql
@@ -1,8 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `theme` to the `WuphfUser` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "WuphfUser" ADD COLUMN     "theme" TEXT NOT NULL;
+UPDATE "WuphfUser" SET "theme" = 'light';
