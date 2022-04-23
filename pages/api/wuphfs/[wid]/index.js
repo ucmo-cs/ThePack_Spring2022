8c060795e828562b0adb45fc74db0c2578cd5749
DESKTOP-3IDEHA2\Zach <zmoeller0912@yahoo.com>
2022-04-23 16:46:41 -0500

    Can no longer update a post to contain all whitespace

--- pages/api/wuphfs/[wid]/index.js
+++ pages/api/wuphfs/[wid]/index.js
@@ -38,43 +38,48 @@ export default async function handler(req, res) {
 			throw error
 		}
 	} else if (req.method === 'PATCH') {
-		const session = await getSession({ req })
-		const user = await prisma.WuphfUser.findUnique({
-			where: {
-				email: session.user.email,
-			},
-		})
-
-		try {
-			const wuphf = await prisma.Wuphf.updateMany({
+		if (req.body.postBody.trim().length === 0) {
+			res.status(400).json({ msg: 'Post cannot contain only white space.' })
+		}
+		else{
+			const session = await getSession({ req })
+			const user = await prisma.WuphfUser.findUnique({
 				where: {
-					id: Number(wid),
-					userId: user.userName,
-				},
-				data: {
-					postBody: req.body.postBody,
-					pictureUrl: req.body.pictureUrl || undefined,
+					email: session.user.email,
 				},
 			})
+		
+			try {
+				const wuphf = await prisma.Wuphf.updateMany({
+					where: {
+						id: Number(wid),
+						userId: user.userName,
+					},
+					data: {
+						postBody: req.body.postBody,
+						pictureUrl: req.body.pictureUrl || undefined,
+					},
+				})
 
-			res.json(wuphf)
-		} catch (error) {
-			// P2025
-			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				res.json(wuphf)
+			} catch (error) {
+				// P2025
+				if (error instanceof Prisma.PrismaClientKnownRequestError) {
 				// The .code property can be accessed in a type-safe manner
-				if (error.code === 'P2025') {
-					return res
-						.status(404)
-						.json({ msg: 'The Wuphf to update was not found' })
+					if (error.code === 'P2025') {
+						return res
+							.status(404)
+							.json({ msg: 'The Wuphf to update was not found' })
+					}
 				}
+				// console.error(error)
+				res.status(500).json({ error })
+				throw error
 			}
-			// console.error(error)
-			res.status(500).json({ error })
-			throw error
-		}
 
 		// #authorization - who is allowed to do this?
 		// #validation - invalid input
+		}
 	} else if (req.method === 'DELETE') {
 		try {
 			const wuphf = await prisma.Wuphf.delete({
