3f4bf33d0b141c95d9f571e2ff8270cb6fddc8c4
Charles Graham <cpg55850@ucmo.edu>
2022-04-23 18:52:13 -0500

    WuphfInput with react-hook-form and yup validation

--- components/timeline/WuphfInput.js
+++ components/timeline/WuphfInput.js
@@ -1,43 +1,83 @@
-import React, { useState } from 'react'
+import React from 'react'
 
+import { yupResolver } from '@hookform/resolvers/yup'
 import axios from 'axios'
+import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
+import { object, string } from 'yup'
 
 import { useWuphfUser } from '../../hooks/WuphfUserContext'
 import Avatar from '../general/Avatar'
 import Button from '../general/Button'
 
+let schema = object({
+	post: string().trim().required('Please enter a Wuphf.'),
+})
+
 function WuphfInput({ addWuphf }) {
-	const [post, setPost] = useState('')
 	const { wuphfUser } = useWuphfUser()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		watch,
+		reset,
+	} = useForm({ resolver: yupResolver(schema) })
 
-	function handleChange(event) {
-		setPost(event.target.value)
-	}
-
-	async function handleSubmit(event) {
-		event.preventDefault()
+	async function postWuphf() {
 		const res = await axios
 			.post('/api/wuphfs', {
 				userName: wuphfUser.userName,
 				pictureUrl: wuphfUser.avatar.url,
-				postBody: post,
+				postBody: watch('post'),
 			})
 			.catch((err) => alert(err))
 
 		if (res) {
 			addWuphf(res.data)
 		}
-		setPost('')
 	}
 
+	function onSubmit(data) {
+		console.log(data)
+		postWuphf()
+		reset()
+	}
+	// console.log('ERRORS!', errors)
+
+	// async function handleSubmit(event) {
+	// 	event.preventDefault()
+
+	// 	// validation
+	// 	try {
+	// 		await wuphfSchema.validate({
+	// 			username: wuphfUser.userName,
+	// 			pictureUrl: wuphfUser.pictureUrl,
+	// 			postBody: post,
+	// 		})
+	// 	} catch (error) {
+	// 		setPost('')
+	// 		return alert(error)
+	// 	}
+
+	// 	const res = await axios
+	// 		.post('/api/wuphfs', {
+	// 			userName: wuphfUser.userName,
+	// 			pictureUrl: wuphfUser.avatar.url,
+	// 			postBody: post,
+	// 		})
+	// 		.catch((err) => alert(err))
+
+	// 	if (res) {
+	// 		addWuphf(res.data)
+	// 	}
+	// 	setPost('')
+	// }
+
 	return (
-		<PostBorder onSubmit={handleSubmit}>
-			<PostTextArea
-				value={post}
-				onChange={handleChange}
-				placeholder="What's happening?"
-			/>
+		<PostBorder onSubmit={handleSubmit(onSubmit)} error={errors.post}>
+			<PostTextArea placeholder="What's happening?" {...register('post')} />
+			{errors && <ErrorMsg>{errors?.post?.message}</ErrorMsg>}
 			<AvatarButtonDiv>
 				<Avatar
 					size='medium'
@@ -53,12 +93,19 @@ function WuphfInput({ addWuphf }) {
 }
 
 const PostBorder = styled.form`
-	border: 1.5px ${(props) => props.theme.colors.border};
+	border: ${(props) =>
+		props.error
+			? '1.5px solid red'
+			: `1.5px solid ${props.theme.colors.border}`};
 	border-style: solid;
 	border-radius: 15px;
 	width: 100%;
 	padding: 1.5rem;
 	/* background-color: ${(props) => props.theme.colors.wuphfInputBackground}; */
+`
+
+const ErrorMsg = styled.span`
+	color: red;
 `
 
 const PostTextArea = styled.textarea`
