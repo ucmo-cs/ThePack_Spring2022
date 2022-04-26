f708d564469984ad1b5212da6231318cbe5024da
Charles Graham <cpg55850@ucmo.edu>
2022-04-26 16:41:22 -0500

    Case insensitive searching for username containing search parameters

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,39 +1,51 @@
-import { getSession } from 'next-auth/react'
 
 import { prisma } from '../../lib/prisma'
 
 export default async function handler(req, res) {
-	const session = await getSession({ req })
+	// const session = await getSession({ req })
 
-	if (session) {
-		if (req.method === 'GET') {
-			try {
-				const users = await prisma.WuphfUser.findMany({
-					where: {
-						OR: [
-							{ userName: { startsWith: req.query.userName } },
-							{ userName: { endsWith: req.query.userName } },
-						],
+	// if (session) {
+	if (req.method === 'GET') {
+		// return res.send()
+		try {
+			const users = await prisma.WuphfUser.findMany({
+				where: {
+					userName: {
+						contains: req.query.userName.trim(),
+						mode: 'insensitive',
 					},
-					select: {
-						userName: true,
-						avatar: {
-							select: {
-								url: true,
-							},
+					// OR: [
+					// 	{
+					// 		userName: {
+					// 			startsWith: ,
+					// 			mode: 'insensitive',
+					// 		},
+					// 	},
+					// 	{
+					// 		userName: { endsWith: req.query.userName, mode: 'insensitive' },
+					// 	},
+					// ],
+				},
+				select: {
+					userName: true,
+					avatar: {
+						select: {
+							url: true,
 						},
 					},
-				})
+				},
+			})
 
-				res.json(users || [])
-			} catch (error) {
-				// console.error(error)
-				res.status(500).json({ error })
-				throw error
-			}
+			res.json(users)
+		} catch (error) {
+			// console.error(error)
+			res.status(500).json({ error })
+			throw error
 		}
-	} else {
-		res.status(401).json({ message: 'User not authenticated' })
 	}
-	res.end()
 }
+// else {
+// 	res.status(401).json({ message: 'User not authenticated' })
+// }
+// res.end()
+// }
